a49eafe47a8f6c229f558e8ea19d9914ce2827ec
Alexis <54474070+AlexisRmnk@users.noreply.github.com>
2019-10-10 14:57:48 -0300

    modificado ejercicio2 agregado ejercicio2_1 agregado bean edadBean

---
+++ TIC_19/src/java/beans/edadBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.Date;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Alexis
+ */
+@Named(value = "edadBean")
+@RequestScoped
+public class edadBean {
+
+    private String nombre;
+    private String apellido;
+    private Date fecha_nacimiento;
+    
+    /**
+     * Creates a new instance of edadBean
+     */
+    public edadBean() {
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+}
